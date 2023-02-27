7d7184ca19031b4697eb6efeb9801c34d0e1e40c
Luis Sastré 🦎 <luis@makingdevs.com>
2023-02-27 12:03:31 -0600

    Move tests from ParserTest to doctest in parser module

--- test/parser_test.exs
+++ test/parser_test.exs
@@ -1,17 +1,5 @@
 defmodule ParserTest do
   use ExUnit.Case
 
-  alias Traefik.Parser
-
-  test "parser the header from request into map" do
-    header_string = ["Accept: */*", "Connection: keep-alive"]
-    headers = Parser.parse_headers(header_string, %{})
-    assert headers == %{"Accept" => "*/*", "Connection" => "keep-alive"}
-  end
-
-  test "parser the params from a request into a map" do
-    params_string = "a=1&b=2&c=3"
-    params = Parser.parse_params("application/x-www-form-urlencoded", params_string)
-    assert params == %{"a" => "1", "b" => "2", "c" => "3"}
-  end
+  doctest Traefik.Parser
 end
