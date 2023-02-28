d303501fcdd46813e83fde2a2a6289abf4535ba6
Luis Sastré 🦎 <luis@makingdevs.com>
2023-02-28 15:00:48 -0600

    Simple server

--- lib/traefik/simple_server.ex
+++ lib/traefik/simple_server.ex
@@ -1,7 +1,18 @@
 defmodule Traefik.SimpleServer do
-  alias Traefik.HttpServer
+  alias Traefik.Handler
 
-  def start(socket) do
-    spawn(HttpServer, :serve, [socket])
+  def start(request) do
+    parent = self()
+    pid = spawn(__MODULE__, :loop, [parent])
+    send(pid, {parent, :call, request})
+  end
+
+  def loop(pid) do
+    receive do
+      {^pid, :call, request} ->
+        request
+        |> Handler.handle()
+        |> IO.inspect(label: :response)
+    end
   end
 end
